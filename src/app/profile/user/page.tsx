e746be69ff1f02bf8697a9c5dc2bae8e0f0bb6cc
GustavoSMelo <gsantos15569@gmail.com>
2025-06-30 22:42:11 -0300

    :construction: working on profile page

--- src/app/profile/user/page.tsx
+++ src/app/profile/user/page.tsx
@@ -1,9 +1,21 @@
 "use client";
 
-import { FaFacebook, FaGithub, FaLock, FaUser } from "react-icons/fa";
+import {
+    FaArchive,
+    FaFacebook,
+    FaGithub,
+    FaLock,
+    FaMoon,
+    FaTrash,
+    FaUser,
+} from "react-icons/fa";
 import "./userprofile.style.css";
 import Footer from "@petnet/components/footer";
 import { AiFillGoogleCircle } from "react-icons/ai";
+import { FaGears } from "react-icons/fa6";
+import { IoMdSunny } from "react-icons/io";
+import { LuDog } from "react-icons/lu";
+import { FiPlusCircle } from "react-icons/fi";
 
 const UserProfile = () => {
     return (
@@ -11,7 +23,10 @@ const UserProfile = () => {
             <main className="mainUserProfileContainer">
                 <div className="userProfileWrapper">
                     <aside>
-                        <FaUser />
+                        <span>
+                            <FaUser className="subtitleIcon" />
+                            <h3>Configuracoes de usuario</h3>
+                        </span>
 
                         <label>Name: </label>
                         <p>Gustavo Melo</p>
@@ -38,23 +53,114 @@ const UserProfile = () => {
                         <ul>
                             <li>
                                 <span>
-                                    <AiFillGoogleCircle className="socialMediaIcon socialMediaIconGoogleConnect" /> Google{" "}
+                                    <AiFillGoogleCircle className="socialMediaIcon socialMediaIconGoogleConnect" />{" "}
+                                    Google{" "}
                                 </span>
                                 <button type="button">Conectar</button>
                             </li>
 
                             <li>
                                 <span>
-                                    <FaFacebook className="socialMediaIcon socialMediaIconFacebookConnect" /> Facebook{" "}
+                                    <FaFacebook className="socialMediaIcon socialMediaIconFacebookConnect" />{" "}
+                                    Facebook{" "}
                                 </span>
-                                <button type="button" className="btnDisconnect">Conectar</button>
+                                <button type="button" className="btnDisconnect">
+                                    Conectar
+                                </button>
                             </li>
 
                             <li>
                                 <span>
-                                    <FaGithub className="socialMediaIcon socialMediaIconGithubConnect" /> Github{" "}
+                                    <FaGithub className="socialMediaIcon socialMediaIconGithubConnect" />{" "}
+                                    Github{" "}
                                 </span>
-                                <button type="button" className="btnConnect">Conectar</button>
+                                <button type="button" className="btnConnect">
+                                    Conectar
+                                </button>
+                            </li>
+                        </ul>
+                    </section>
+                </div>
+
+                <div className="userProfileWrapper">
+                    <aside className="accountConfigWrapper">
+                        <span>
+                            <FaGears className="subtitleIcon" />
+                            <h3>Configuracoes de conta: </h3>
+                        </span>
+
+                        <div className="accountInitialPageConfig">
+                            <label>Pagina inicial: </label>
+                            <select>
+                                <option>🏡 Home</option>
+                                <option>📷 Galeria</option>
+                                <option>📱 Feed</option>
+                                <option>🕹 Atividades</option>
+                            </select>
+                        </div>
+
+                        <label>Tema: </label>
+                        <div className="switchContainer">
+                            <p>
+                                <IoMdSunny />
+                            </p>
+                            <button className="btnSwitch btnSwitchLightTheme">
+                                <span className=""></span>
+                            </button>
+                            <p>
+                                <FaMoon />
+                            </p>
+                        </div>
+
+                        <div className="btnContainer">
+                            <button className="btnDisable" type="button">
+                                <FaArchive className="btnIcons" /> Desativar
+                                conta
+                            </button>
+                            <button className="btnRemove" type="button">
+                                <FaTrash className="btnIcons" /> Excluir conta
+                            </button>
+                        </div>
+                    </aside>
+
+                    <section className="petsContainer">
+                        <h3>
+                            <LuDog className="subtitleIcon" /> Pets:
+                        </h3>
+
+                        <ul>
+                            <li className="listItemPet">
+                                <figure>
+                                    <img
+                                        src="/assets/catface.jpg"
+                                        alt="image"
+                                    />
+                                    <figcaption>petname</figcaption>
+                                </figure>
+
+                                <div>
+                                    <button className="btnDetails">
+                                        Detalhes
+                                    </button>
+                                </div>
+                            </li>
+                            <li>
+                                <figure>
+                                    <img
+                                        src="/assets/catface.jpg"
+                                        alt="image"
+                                    />
+                                    <figcaption>petname</figcaption>
+                                </figure>
+
+                                <div>
+                                    <button className="btnDetails">
+                                        Detalhes
+                                    </button>
+                                </div>
+                            </li>
+                            <li className="addPet">
+                                <FiPlusCircle />
                             </li>
                         </ul>
                     </section>
